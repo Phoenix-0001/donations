8e5c0e16be46c646e3d688e948cf6884845271af
aayalternate <aayalternate@gmail.com>
2024-04-05 23:59:53 +0530

    next sign in update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,32 +6,34 @@ function App() {
   const [password, setPassword] = useState("");
   return (
     <div className="flex justify-center align-middle mt-96">
-      <div className="w-2/5 border-2 px-5 pt-7 pb-5">
+      <div className="w-1/5 border-2 px-5 pt-7 pb-5">
         SIGN IN
         <div className="pt-1">Enter your details</div>
-        <div className="flex flex-row">
+        <div className="flex flex-col items-center">
+        <div className="flex flex-row justify-between w-full">
           NAME
           <input
             type="email"
-            className="border-2"
+            className="border-2 w-4/5"
             onChange={(e) => {
               setEmail(e.target.value);
             }}
           />
         </div>
 
-        <div>
+        <div className="flex flex-row justify-between w-full">
           Email
           <input
             type="password"
-            className="border-2"
+            className="border-2 w-4/5"
             onChange={(e) => {
               setPassword(e.target.value);
             }}
           />
         </div>
+        </div>
 
-        <div>
+        <div className="flex flex-row  justify-between w-25">
           <div>Already have an account?</div>
           <button
             onClick={() => {
