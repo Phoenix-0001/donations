0e072a6c302a0f5bd3a00d71f6a851185848de87
benhur <benhurbenny@iclould.com>
2024-04-06 00:13:17 +0530

    something

---
+++ frontend/src/pages/signUpPage.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+export const SignInComponent = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="flex justify-center align-middle mt-96">
+      <div className="w-1/5 border-2 px-5 pt-7 pb-5">
+        SIGN IN
+        <div className="pt-1">Enter your details</div>
+        <div className="flex flex-col items-center">
+          <div className="flex flex-row justify-between w-full">
+            NAME
+            <input
+              type="email"
+              className="border-2 w-4/5"
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
+          </div>
+
+          <div className="flex flex-row justify-between w-full">
+            Email
+            <input
+              type="password"
+              className="border-2 w-4/5"
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </div>
+        </div>
+        <div className="flex flex-row  justify-between w-25">
+          <div>Already have an account?</div>
+          <button
+            onClick={() => {
+              console.log(email);
+              console.log(password);
+            }}
+          >
+            create an account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
