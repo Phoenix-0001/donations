529bf8b2d6dd21d170573609c3532070f2014927
benhur <benhurbenny@iclould.com>
2024-04-05 23:19:17 +0530

    something

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,44 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  return (
+    <div className="flex justify-center align-middle mt-96">
+      <div className="w-2/5 border-2">
+        signin
+        <div>Enter your details</div>
+        <div className="flex flex-col">
+          <input
+            type="email"
+            className="border-2"
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            className="border-2"
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+          />
+        </div>
+        <div>
+          <div>Already have an account?</div>
+          <button
+            onClick={() => {
+              console.log(email);
+              console.log(password);
+            }}
+          >
+            create an account
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
